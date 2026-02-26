0df4ab5267f33a180579e1c94cadf6e3a72b5a08
Rohail-Zuberi <rohailzuberi5@gmail.com>
2026-02-26 20:33:05 +0500

    remove prebuilt vercel output and ignore .vercel

--- .vercel/output/static/_astro/hoisted.BGWVWotS.js
+++
@@ -1,1 +0,0 @@
-import"./main.astro_astro_type_script_index_0_lang.DF2keTmI.js";import"./main.astro_astro_type_script_index_1_lang.CbgSXvim.js";import"./index.astro_astro_type_script_index_0_lang.C06vs49o.js";import"./index.astro_astro_type_script_index_0_lang.n4tcr7eN.js";
